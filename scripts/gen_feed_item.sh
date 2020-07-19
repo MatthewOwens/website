d6bbbd2f9cdef1bf96454e77c5e0663d9d6074f5
MatthewOwens <matthew@owens.tech>
2020-07-19 13:23:44 +0100

    feed dates

--- scripts/gen_feed_item.sh
+++ scripts/gen_feed_item.sh
@@ -4,10 +4,11 @@
 HTMLPATH=$(echo $1 | sed 's/\.md$/.html/')
 TITLE=$(scripts/post_title.sh $1)
 BODY=$(markdown $1 | sed -e 's/&rsquo;/'/g -e 's/&lsquo;//g')
+DATE=$(echo $1 | awk -F[/-] '{ printf "%s %s %s", $2, $3, $4 }' | date -Ru)
 
 echo "<item>"
 echo "	<title>$TITLE</title>"
 echo "	<guid>https://owens.tech/posts/$HTMLPATH</guid>"
-echo "	<pubDate>TODO</pubDate>"
+echo "	<pubDate>$DATE</pubDate>"
 echo "	<description>$BODY</description>" 
 echo "</item>" 
