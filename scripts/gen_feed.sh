b8032d29f5f3c0913a176d310e53460ab919b8db
MatthewOwens <matthew@owens.tech>
2020-07-19 13:48:55 +0100

    removed unneeded parameter

--- scripts/gen_feed.sh
+++ scripts/gen_feed.sh
@@ -13,7 +13,7 @@ printf '\t<atom:link href="https://owens.tech/rss.xml" rel="self" type="applicat
 # generate
 
 printf '\n\n'
-find posts/ -type f -exec sh -c "scripts/gen_feed_item.sh {} $1" \;
+find posts/ -type f -exec sh -c "scripts/gen_feed_item.sh {}" \;
 printf '\n\n'
 
 printf '</channel>\n'
