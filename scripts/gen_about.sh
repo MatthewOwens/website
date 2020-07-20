f27d1d61d42aebe77cb8c9a6b874789baa805432
MatthewOwens <matthew@owens.tech>
2020-07-20 13:08:55 +0100

    using correct header for about & project pages

---
+++ scripts/gen_about.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+[ $# -ne 1 ] && exit 1
+
+title=$(scripts/post_title.sh $1)
+
+# since $title will have a closing HTML tag, we need to use a different
+# seperator to '/' for sed
+sed "s|TITLE|$title|" includes/head.html
+cat includes/about_header.html
+markdown $1
