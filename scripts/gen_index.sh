8bcdd37dfa8e3ae177bbc8f74b7ba7025b58ed0e
MatthewOwens <matthew@owens.tech>
2020-07-21 16:59:53 +0100

    about div

--- scripts/gen_index.sh
+++ scripts/gen_index.sh
@@ -9,4 +9,6 @@ MATCH="^([0-9]{1,4})-([0-9]{1,4})-([0-9]{1,4})-([^\.]+)(.*)"
 sed "s|TITLE|<title>Matthew Owens</title>|" includes/head.html
 cat includes/header.html
 ls $POSTS_DIR | sed -Ee 's/ /\n/' -e "s/$MATCH/\1 \2 \3 \4/" | awk -f scripts/posts.awk
+echo "<div id=\"about-section\">"
 markdown about.md
+echo "</div>"
