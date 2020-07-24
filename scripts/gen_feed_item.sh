af51d3abc933707a6cba2d5ec9a1ee44492bcac7
MatthewOwens <matthew@owens.tech>
2020-07-24 09:38:52 +0100

    updated script to fix single and double quotes dying with xml

--- scripts/gen_feed_item.sh
+++ scripts/gen_feed_item.sh
@@ -3,7 +3,7 @@
 
 HTMLPATH=$(echo $1 | sed 's/\.md$/.html/')
 TITLE=$(scripts/post_title.sh $1)
-BODY=$(markdown $1 | sed -e 's/&rsquo;/'/g -e 's/&lsquo;//g')
+BODY=$(markdown $1 | sed -e "s/&[rl]squo;/\'"/g -e 's/&[rl]dquo;/\"/g')
 DATE=$(git log -1 --pretty="format:%ad" $1)
 
 echo "<item>"
